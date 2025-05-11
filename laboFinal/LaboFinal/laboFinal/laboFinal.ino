83781e56595e1d668ccb00b0de5b9b484f75543a
cuzimapanda2003 <marcblais03@gmail.com>
2025-05-11 12:31:03 -0400

    Update laboFinal.ino

--- laboFinal/LaboFinal/laboFinal/laboFinal.ino
+++ laboFinal/LaboFinal/laboFinal/laboFinal.ino
@@ -56,7 +56,7 @@ unsigned long tempsAffichageSymbole = 0;
 bool symboleActif = false;
 
 
-#define HAS_SECRETS 0
+#define HAS_SECRETS 1
 
 #if HAS_SECRETS
 #include "arduino_secrets.h"
@@ -73,7 +73,7 @@ const char pass[] = "shawi123";                         // your network password
 #endif
 
 #if HAS_SECRETS
-#define DEVICE_NAME "2168637Maison"
+#define DEVICE_NAME "2168637"
 #else
 #define DEVICE_NAME "2168637"
 #endif
@@ -81,6 +81,8 @@ const char pass[] = "shawi123";                         // your network password
 #define MQTT_PORT 1883
 #define MQTT_USER "etdshawi"
 #define MQTT_PASS "shawi123"
+
+#define AT_BAUD_RATE 115200
 
 // Serveur MQTT du prof
 const char* mqttServer = "216.128.180.194";
@@ -92,38 +94,94 @@ unsigned long lastWifiAttempt = 0;
 const unsigned long wifiRetryInterval = 10000;
 
 void wifiInit() {
-  while (!Serial)
-    ;
+ while (!Serial);
 
-  Serial1.begin(115200);
-  WiFi.init(&Serial1);
+  Serial1.begin(AT_BAUD_RATE);
+  WiFi.init(Serial1);
 
   if (WiFi.status() == WL_NO_MODULE) {
     Serial.println();
-    Serial.println("La communication avec le module WiFi a échoué!");
+    Serial.println("Communication with WiFi module failed!");
+    // don't continue
+    while (true);
   }
 
-  WiFi.disconnect();  // pour effacer le chemin. non persistant
+  WiFi.disconnect(); // to clear the way. not persistent
 
-  WiFi.setPersistent();  // définir la connexion WiFi suivante comme persistante
+  WiFi.setPersistent(); // set the following WiFi connection as persistent
 
-  WiFi.endAP();  // pour désactiver le démarrage automatique persistant AP par défaut au démarrage
+  WiFi.endAP(); // to disable default automatic start of persistent AP at startup
+
+//  uncomment this lines for persistent static IP. set addresses valid for your network
+//  IPAddress ip(192, 168, 1, 9);
+//  IPAddress gw(192, 168, 1, 1);
+//  IPAddress nm(255, 255, 255, 0);
+//  WiFi.config(ip, gw, gw, nm);
 
   Serial.println();
-  Serial.print("Tentative de connexion à SSID: ");
+  Serial.print("Attempting to connect to SSID: ");
   Serial.println(ssid);
+
+//  use following lines if you want to connect with bssid
+//  const byte bssid[] = {0x8A, 0x2F, 0xC3, 0xE9, 0x25, 0xC0};
+//  int status = WiFi.begin(ssid, pass, bssid);
 
   int status = WiFi.begin(ssid, pass);
 
   if (status == WL_CONNECTED) {
     Serial.println();
-    Serial.println("Connecté au réseau WiFi.");
+    Serial.println("Connected to WiFi network.");
+    printWifiStatus();
   } else {
-    WiFi.disconnect();
+    WiFi.disconnect(); // remove the WiFi connection
     Serial.println();
-    Serial.println("La connexion au réseau WiFi a échoué.");
+    Serial.println("Connection to WiFi network failed.");
   }
 }
+
+void printWifiStatus() {
+
+  // print the SSID of the network you're attached to:
+  char ssid[33];
+  WiFi.SSID(ssid);
+  Serial.print("SSID: ");
+  Serial.println(ssid);
+
+  // print the BSSID of the network you're attached to:
+  uint8_t bssid[6];
+  WiFi.BSSID(bssid);
+  Serial.print("BSSID: ");
+  printMacAddress(bssid);
+
+  uint8_t mac[6];
+  WiFi.macAddress(mac);
+  Serial.print("MAC: ");
+  printMacAddress(mac);
+
+  // print your board's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm");
+}
+void printMacAddress(byte mac[]) {
+  for (int i = 5; i >= 0; i--) {
+    if (mac[i] < 16) {
+      Serial.print("0");
+    }
+    Serial.print(mac[i], HEX);
+    if (i > 0) {
+      Serial.print(":");
+    }
+  }
+  Serial.println();
+}
+
 
 void toggleMoteur() {
   static bool moteurActif = false;
@@ -148,7 +206,7 @@ void mqttEvent(char* topic, byte* payload, unsigned int length) {
   }
   Serial.println();
 
-  if (strcmp(topic, "moteur") == 0) {
+  if (strcmp(topic, "etd/20/motor") == 0) {
     toggleMoteur();
   }
 }
@@ -186,7 +244,7 @@ void periodicTask() {
     reconnect();
     Serial.println("Échec d'envoi !");
   } else {
-    Serial.println("Message envoyé avec succès.");
+    Serial.println("Message envoyé");
   }
 }
 
